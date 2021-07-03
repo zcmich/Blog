5211cd3e56b95d60e21850dfba0c49caf795d4be
zcmich <zafackmichel@yahoo.fr>
2021-07-03 20:35:17 +0100

    laracast Blog

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/app.css">
+    <title>My Blog</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem in suscipit nulla totam deleniti iusto magni numquam esse quo, similique, perferendis quos enim fugit ipsam et quidem! Aliquam ea eligendi animi et nesciunt cum perspiciatis, dicta repudiandae placeat eius, possimus maiores sed alias quisquam inventore esse voluptatibus. Exercitationem, at dicta incidunt qui dolores vero dolore, itaque, culpa quam quasi porro omnis. Quae hic cumque fuga labore voluptate eos quod, doloremque veritatis vero. Deserunt at tempore modi sint! Libero vel ipsam mollitia, eaque accusantium eum labore ut maiores corrupti? Porro impedit, delectus nesciunt vero exercitationem laudantium architecto explicabo modi atque earum laborum deleniti numquam sapiente, eveniet optio repellendus incidunt? Sequi aperiam architecto nihil quam eum animi iusto repudiandae, maiores ducimus quaerat deserunt, adipisci officia distinctio sed, sapiente culpa vero incidunt! Ipsum ipsa aut asperiores perspiciatis! Provident quam ad, nam molestiae quidem repellat eius enim et eos error, omnis beatae? Nemo qui optio eveniet necessitatibus tempora, cum libero recusandae minima veritatis molestiae ullam unde earum asperiores possimus nulla magnam beatae omnis autem culpa? Illo repellat sint deserunt obcaecati sequi tempora temporibus, blanditiis reiciendis omnis eius totam? Eos doloremque pariatur possimus tenetur recusandae ab ratione magnam perspiciatis eius ex hic, cumque, enim repellendus.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem in suscipit nulla totam deleniti iusto magni numquam esse quo, similique, perferendis quos enim fugit ipsam et quidem! Aliquam ea eligendi animi et nesciunt cum perspiciatis, dicta repudiandae placeat eius, possimus maiores sed alias quisquam inventore esse voluptatibus. Exercitationem, at dicta incidunt qui dolores vero dolore, itaque, culpa quam quasi porro omnis. Quae hic cumque fuga labore voluptate eos quod, doloremque veritatis vero. Deserunt at tempore modi sint! Libero vel ipsam mollitia, eaque accusantium eum labore ut maiores corrupti? Porro impedit, delectus nesciunt vero exercitationem laudantium architecto explicabo modi atque earum laborum deleniti numquam sapiente, eveniet optio repellendus incidunt? Sequi aperiam architecto nihil quam eum animi iusto repudiandae, maiores ducimus quaerat deserunt, adipisci officia distinctio sed, sapiente culpa vero incidunt! Ipsum ipsa aut asperiores perspiciatis! Provident quam ad, nam molestiae quidem repellat eius enim et eos error, omnis beatae? Nemo qui optio eveniet necessitatibus tempora, cum libero recusandae minima veritatis molestiae ullam unde earum asperiores possimus nulla magnam beatae omnis autem culpa? Illo repellat sint deserunt obcaecati sequi tempora temporibus, blanditiis reiciendis omnis eius totam? Eos doloremque pariatur possimus tenetur recusandae ab ratione magnam perspiciatis eius ex hic, cumque, enim repellendus.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem in suscipit nulla totam deleniti iusto magni numquam esse quo, similique, perferendis quos enim fugit ipsam et quidem! Aliquam ea eligendi animi et nesciunt cum perspiciatis, dicta repudiandae placeat eius, possimus maiores sed alias quisquam inventore esse voluptatibus. Exercitationem, at dicta incidunt qui dolores vero dolore, itaque, culpa quam quasi porro omnis. Quae hic cumque fuga labore voluptate eos quod, doloremque veritatis vero. Deserunt at tempore modi sint! Libero vel ipsam mollitia, eaque accusantium eum labore ut maiores corrupti? Porro impedit, delectus nesciunt vero exercitationem laudantium architecto explicabo modi atque earum laborum deleniti numquam sapiente, eveniet optio repellendus incidunt? Sequi aperiam architecto nihil quam eum animi iusto repudiandae, maiores ducimus quaerat deserunt, adipisci officia distinctio sed, sapiente culpa vero incidunt! Ipsum ipsa aut asperiores perspiciatis! Provident quam ad, nam molestiae quidem repellat eius enim et eos error, omnis beatae? Nemo qui optio eveniet necessitatibus tempora, cum libero recusandae minima veritatis molestiae ullam unde earum asperiores possimus nulla magnam beatae omnis autem culpa? Illo repellat sint deserunt obcaecati sequi tempora temporibus, blanditiis reiciendis omnis eius totam? Eos doloremque pariatur possimus tenetur recusandae ab ratione magnam perspiciatis eius ex hic, cumque, enim repellendus.</p>
+    </article>
+</body>
+</html>
