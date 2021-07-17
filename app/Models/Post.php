cf4c1abbd98295ff978c6dd201b41b816f2f0300
zcmich <zafackmichel@yahoo.fr>
2021-07-17 21:11:22 +0100

    laracast eps 13

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -28,19 +28,23 @@ class Post
 
     public static function all()
     {
-        return collect(File::files(resource_path("posts")))
-            ->map(function ($file) {
-                return YamlFrontMatter::parseFile($file);
-            })
-            ->map(function ($document){
-                return new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug
-                );
-            });
+        return cache()->rememberForever('posts.all',function(){
+            return collect(File::files(resource_path("posts")))
+                ->map(function ($file) {
+                    return YamlFrontMatter::parseFile($file);
+                })
+                ->map(function ($document){
+                    return new Post(
+                        $document->title,
+                        $document->excerpt,
+                        $document->date,
+                        $document->body(),
+                        $document->slug
+                    );
+                })->sortByDesc(('date'));
+        });
+
+
 
 //    $posts = collect($files)->map(function ($file){
 //        $document = YamlFrontMatter::parseFile($file);
