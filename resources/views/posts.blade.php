323d728d60006ce13b0a0157db068370d5f8a9b1
zcmich <zafackmichel@yahoo.fr>
2021-08-22 22:19:07 +0100

    eps 32

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,24 +1,37 @@
 <x-layout>
+   @include('_posts-header')
 
-    @foreach ($posts as $post)
-        {{--        @dd($loop)--}}
-        <article>
-            <h1>
-{{--                        @dd($post)--}}
-                <a href="/posts/{{$post->slug }}">
-                    {!! $post->title !!}
-                </a>
-            </h1>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
 
-{{--            @dd($post)--}}
-            <p>
-            By <a href="/authors/{{ $post->author->username}}">{{ $post->author->name }}</a>   <a href="/categories/{{ $post->category->slug}}">{{ $post->category->name }}</a>
-            </p>
+               <x-posts-grid :posts="$posts" />
 
-            <div>
-                {!! $post->excerpt !!}
-            </div>
-        </article>
-    @endforeach
+        @else
+            <p class="text-center">  No posts yet.Please check back later. </p>
+        @endif
+
+
+    </main>
+
+    {{--    @foreach ($posts as $post)--}}
+    {{--        --}}{{--        @dd($loop)--}}
+    {{--        <article>--}}
+    {{--            <h1>--}}
+    {{--                        @dd($post)--}}
+    {{--                <a href="/posts/{{$post->slug }}">--}}
+    {{--                    {!! $post->title !!}--}}
+    {{--                </a>--}}
+    {{--            </h1>--}}
+
+    {{--            @dd($post)--}}
+    {{--            <p>--}}
+    {{--            By <a href="/authors/{{ $post->author->username}}">{{ $post->author->name }}</a>   <a href="/categories/{{ $post->category->slug}}">{{ $post->category->name }}</a>--}}
+    {{--            </p>--}}
+
+    {{--            <div>--}}
+    {{--                {!! $post->excerpt !!}--}}
+    {{--            </div>--}}
+    {{--        </article>--}}
+    {{--    @endforeach--}}
 
 </x-layout>
