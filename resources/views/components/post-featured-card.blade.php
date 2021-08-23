ab963bf6cc9b0dfbb09c0e09999b35cfc1b1b281
zcmich <zafackmichel@yahoo.fr>
2021-08-23 22:07:59 +0100

    eps32

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -4,7 +4,7 @@
     <div class="py-6 px-5 lg:flex">
         <div class="flex-1 lg:mr-8">
           {{-- TODO --}}
-            <img src="./images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
+            <img src="/images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
         </div>
 
         <div class="flex-1 flex flex-col justify-between">
@@ -17,8 +17,11 @@
 
                 <div class="mt-4">
                     <h1 class="text-3xl">
-                        <a href=" /post/{{ $post->slug }} "></a>
-                        {{ $post->title }}
+{{--                        @dd($post)--}}
+                        <a href="/posts/{{$post->slug}}">
+                            {{ $post->title }}
+                        </a>
+
                     </h1>
 
                     <span class="mt-2 block text-gray-400 text-xs">
@@ -36,7 +39,7 @@
 
             <footer class="flex justify-between items-center mt-8">
                 <div class="flex items-center text-sm">
-                    <img src="./images/lary-avatar.svg" alt="Lary avatar">
+                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
                         <h5 class="font-bold">{{ $post->author->name }}</h5>
                         <h6>Mascot at Laracasts</h6>
