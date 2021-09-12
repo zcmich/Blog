d49a9ab4601707a99989ea6078f46e24f7572db8
zcmich <zafackmichel@yahoo.fr>
2021-09-12 16:39:55 +0100

    section 9 & pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,8 @@ class PostController extends Controller
 
 //    @dd(request('search'));
        return view('posts.index', [
-           'posts' => Post::latest()->filter(request(['search','category','author']))->get()
+           'posts' => Post::latest()->filter(
+               request(['search','category','author']))->paginate(6)->withQueryString()
 //           'categories' => Category::all(), now passed in the CategoryDropdown
        ]);
    }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,6 +6,8 @@
 
                <x-posts-grid :posts="$posts" />
 
+            {{ $posts->links() }}
+
         @else
             <p class="text-center">  No posts yet.Please check back later. </p>
         @endif
